993776b1bf57648f7ec7812f827ef84c41777d5a
justanr <alecreiter@gmail.com>
2015-05-26 12:29:41 -0400

    Move with_metaclass to compatibility module

--- objtoolz/compat.py
+++ objtoolz/compat.py
@@ -6,6 +6,27 @@ try:
 except ImportError:
     from singledispatch import singledispatch
 
+
+
+def with_metaclass(meta, *bases, **kwargs):
+    """Allows inheriting from an anonymous object with the specified metaclass.
+    If a metaclass allows optional or required keyword arguments they can be
+    provided in this function.
+
+    Modified from Armin Ronacher's spin on six.py's with_metaclass
+    """
+    class metaclass(meta):
+        __call__ = type.__call__
+        __init__ = type.__init__
+
+        def __new__(cls, name, this_bases, d):
+            if this_bases is None:
+                return type.__new__(cls, name, (), d)
+            return meta(name, bases, d, **kwargs)
+
+    return metaclass('temporary_class', None, {})
+
+
 def update_wrapper(wrapper, wrapped,
                    assigned=WRAPPER_ASSIGNMENTS, updated=WRAPPER_UPDATES):
     for attr in assigned:
--- objtoolz/metas/__init__.py
+++ objtoolz/metas/__init__.py
@@ -16,22 +16,3 @@ class CurriedMemoized(Curried, Memoized):
     it is Memoized.
     """
     pass
-
-
-def with_metaclass(meta, *bases, **kwargs):
-    """Allows inheriting from an anonymous object with the specified metaclass.
-    If a metaclass allows optional or required keyword arguments they can be
-    provided in this function.
-
-    Modified from Armin Ronacher's spin on six.py's with_metaclass
-    """
-    class metaclass(meta):
-        __call__ = type.__call__
-        __init__ = type.__init__
-
-        def __new__(cls, name, this_bases, d):
-            if this_bases is None:
-                return type.__new__(cls, name, (), d)
-            return meta(name, bases, d, **kwargs)
-
-    return metaclass('temporary_class', None, {})
